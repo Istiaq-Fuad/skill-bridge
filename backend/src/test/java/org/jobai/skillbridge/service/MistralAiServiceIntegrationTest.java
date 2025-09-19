25a6c0a0a471568f8e686792e7cef4b59f1c9484
AzadMahmud <azadmahmud2k@gmail.com>
2025-09-19 15:06:09 +0600

    Fix import in MistralAiServiceIntegrationTest

--- backend/src/test/java/org/jobai/skillbridge/service/MistralAiServiceIntegrationTest.java
+++ backend/src/test/java/org/jobai/skillbridge/service/MistralAiServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package org.jobai.skillbridge.service;
 
+import org.jobai.skillbridge.dto.AiResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
